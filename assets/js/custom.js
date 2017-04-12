dbc0a52c0f34a852699041008770d97f4040afb7
Alex <kochnevalex@gmail.com>
2017-04-12 22:45:44 +0300

    madking landing

---
+++ assets/js/custom.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+
+    var btnActiv = function () {
+        var $btn = $('.btn-wrap .btn');
+        $btn.hover(function () {
+            $(this).siblings().toggleClass('active');
+        });
+    };
+
+    var bgChange = function () {
+        var $btnRed = $('.btn-wrap .btn--red'),
+            $btnBlue = $('.btn-wrap .btn--blue'),
+            $bgMain = $('.bg-main'),
+            $bgLeft = $('.bg-left'),
+            $bgRight = $('.bg-right');
+        $btnRed.hover(function () {
+            $bgRight.toggleClass('active');
+        });
+        $btnBlue.hover(function () {
+            $bgLeft.toggleClass('active');
+        });
+    };
+
+    var bgImage = function () {
+
+        if ($('.bg-image').length) {
+            $('.bg-image').each(function () {
+                var $node = $(this);
+                var imgUrl = $node.data('bgimage');
+                $node.css({
+                    backgroundImage: "url('" + imgUrl + "')"
+                });
+            });
+        }
+
+    };
+
+
+    var popup = function () {
+        var popupOpen = $('.btn--popup');
+        popupOpen.magnificPopup({
+            showCloseBtn: false,
+            type: 'inline',
+            padding: 0,
+            midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        });
+    };
+
+
+    var validation = function () {
+        var $formSender = $('#form');
+        $formSender.validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                nick: {
+                    required: true,
+                    minlength: 4
+                },
+                pass: {
+                    required: true,
+                    minlength: 6
+                }
+            },
+            messages: {
+                email: {
+                    required: 'Корректный адрес email имеет формат nick@mail.ru.',
+                    email: 'Корректный адрес email имеет формат nick@mail.ru.'
+                },
+                nick: {
+                    required: 'Никнейм должен содержать 4 и более символов.',
+                    minlength: 'Никнейм должен содержать 4 и более символов.'
+                },
+                pass: {
+                    required: 'Пароль должен содержать 6 и более символов.',
+                    minlength: 'Пароль должен содержать 6 и более символов.'
+                }
+
+            },
+
+        });
+    };
+
+
+    validation();
+    popup();
+    bgChange();
+    btnActiv();
+    bgImage();
+
+});
